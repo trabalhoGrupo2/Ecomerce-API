b6dcb474669195c9b3886e94399ba8d51b570596
Maria Aragao <maria.s.aragao.2025@gmail.com>
2025-05-27 19:34:18 -0300

    Criada as classes cliente, fornecedor, conta e funcionário

---
+++ src/main/java/org/serratec/h2/grupo2/Conta/domain/NivelAcesso.java
@@ -0,0 +1,6 @@
+package org.serratec.h2.grupo2.Conta.domain;
+
+public enum NivelAcesso {
+
+	BAIXO, MEDIO, ALTO, TOTAL
+}
---
+++ src/main/java/org/serratec/h2/grupo2/fornecedor/TipoPessoa.java
@@ -0,0 +1,6 @@
+package org.serratec.h2.grupo2.fornecedor;
+
+public enum TipoPessoa {
+	
+	FISICA, JURIDICA
+}
