2458efbfe6c3e569b2a95b213d8075f63262a75a
Cauã Pacheco <pachecocaua7@gmail.com>
2025-05-27 14:29:27 -0300

    Primeiro commit

---
+++ src/test/java/org/serratec/h2/grupo2/Grupo2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.serratec.h2.grupo2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Grupo2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
